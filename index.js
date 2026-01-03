6356e05d961953f746a9d75cbb645e738326ee40
Adilson A. Mattos <adilson.day@gmail.com>
2026-01-02 23:32:04 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors'); // 1. Importa o CORS
+const routes = require('./src/routes')
+const { disconnectDB } = require('./src/config/DbClient');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+// Captura o sinal de interrupção (Ctrl + C)
+process.on('SIGINT', async () => {
+    await disconnectDB();
+    process.exit(0);
+});
+
+// Captura o sinal de encerramento (usado por serviços de hospedagem)
+process.on('SIGTERM', async () => {
+    await disconnectDB();
+    process.exit(0);
+});
