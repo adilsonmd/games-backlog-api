900ba14e221a358b65d05bad6a86fb81f2c738c9
Adilson A. Mattos <adilson.day@gmail.com>
2026-01-02 23:57:52 -0300

    Remove Nintendo

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ const PsnRoutes = require('./PsnRoutes');
 const GameRoutes = require('./GameRoutes');
 const SteamRoutes = require('./SteamRoutes');
 const SettingRoutes = require('./SettingRoutes');
-const NintendoRoutes = require('./NintendoRoutes');
 // const usuarioRoutes = require('./usuarioRoutes'); // Exemplo futuro
 
 // Agrupa as rotas por prefixo
@@ -17,7 +16,6 @@ router.use('/setting/', SettingRoutes);
 // Rotas de terceiros
 router.use('/psn/', PsnRoutes);
 router.use('/steam/', SteamRoutes);
-router.use('/nintendo/', NintendoRoutes);
 
 
 module.exports = router;
