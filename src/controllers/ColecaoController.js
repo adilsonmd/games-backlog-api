93d7776be4bbe9ab58ff2b48f3402a3e89e90b23
Adilson A. Mattos <adilson.day@gmail.com>
2026-02-14 15:22:37 -0300

    Ajustes fnção de coleção

--- src/controllers/ColecaoController.js
+++ src/controllers/ColecaoController.js
@@ -4,6 +4,28 @@ const getAll = async (req, res) => {
      try {
         const colecoes = await ColecaoSchema.find().sort({ nome: 1 }).exec();
 
+        if (!colecoes) {
+            res.status(400).json({});
+        }
+
+        for (let cc in colecoes) {
+            if (!cc.games) {
+                continue;
+            }
+
+            let numeroJogos = cc.games.length;
+            let finalizados = cc.games.reduce((acc, game) => {
+                if (game.status == 'Finalizado')
+                    return acc + 1
+                return acc;
+            }, 0);
+            
+
+            let valor = (finalizados / numeroJogos) * 100;
+
+            cc.progresso = valor;
+        }
+
         res.status(200).json(colecoes);
     } catch (error) {
         res.status(400).json({ erro: "Erro ao listar coleções. " + error });
@@ -11,9 +33,9 @@ const getAll = async (req, res) => {
 }
 const getId = async (req, res) => {
     try {
-        const colecaoId = req.params.colecaoId;
+        const colecaoId = req.params._id;
 
-        const colecao = await ColecaoSchema.find({ colecaoId: colecaoId }).exec();
+        const colecao = await ColecaoSchema.findById(colecaoId).exec();
 
         res.status(200).json(colecao);
     } catch (error) {
@@ -22,6 +44,8 @@ const getId = async (req, res) => {
 };
 const create = async (req, res) => {
     try {
+        console.log("Create", req.body);
+
         const colecao = await ColecaoSchema.create(req.body);
         res.status(201).json(colecao);
     }
