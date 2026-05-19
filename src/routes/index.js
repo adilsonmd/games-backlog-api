410c0511f09ff210c1ebbffc79e9f766b0b04faa
Adilson A. Mattos <adilson.day@gmail.com>
2026-05-19 12:00:15 -0300

    adiciona rota auth basica usando authelia

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,7 +18,17 @@ const setupDatabase = require('../middlewares/setupDatabase');
 // Agrupa as rotas por prefixo
 router.use(setupDatabase);
 
-//router.use('/auth/', AuthRoutes);
+router.get('/auth', (req, res) => {
+  const username = req.headers['x-authelia-username'];
+  const email = req.headers['x-authelia-email'];
+
+  if (!username) {
+    return res.status(401).json({ error: 'Não autorizado' });
+  }
+
+  // Devolve os dados para o frontend usar se quiser exibir o nome na tela
+  res.json({ username, email });
+});
 
 // Rotas principais 
 router.use('/games/', authMiddleware, GameRoutes);
